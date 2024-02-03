cece36d70cd810781774c340f6cc218468f68d3c
ennap <ennap@LAPTOP-LI9S114J>
2024-02-03 21:34:42 +0530

    Added logo validation test case

---
+++ Test Suites/DDT_TestSuit/Testlistener_Testsuites/TC_Amazon_validating_Logo_004.ts
@@ -0,0 +1,21 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description></description>
+   <name>TC_Amazon_validating_Logo_004</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>3</numberOfRerun>
+   <pageLoadTimeout>30</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <rerunImmediately>true</rerunImmediately>
+   <testSuiteGuid>6f60d694-03df-40ca-8f9e-32896e5a7b57</testSuiteGuid>
+   <testCaseLink>
+      <guid>0e9293d2-7352-4395-8d87-b530aa47adf4</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Amazon_testcases/TC_Amazon_validating_Logo_004</testCaseId>
+      <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
+   </testCaseLink>
+</TestSuiteEntity>
