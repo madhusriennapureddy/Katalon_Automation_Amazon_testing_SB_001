bd4d5030ba11af9b830113855bcd73681e3fdc9b
ennap <ennap@LAPTOP-LI9S114J>
2024-02-03 21:44:26 +0530

    Added validation of login

---
+++ Test Suites/DDT_TestSuit/Testlistener_Testsuites/TC_Amazon_validating_Login_005.ts
@@ -0,0 +1,21 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description></description>
+   <name>TC_Amazon_validating_Login_005</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>3</numberOfRerun>
+   <pageLoadTimeout>30</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <rerunImmediately>true</rerunImmediately>
+   <testSuiteGuid>fabf3b58-e135-4de3-9380-648a0e9ca61f</testSuiteGuid>
+   <testCaseLink>
+      <guid>64b2d262-495a-4828-a0f0-e3ebd287712f</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Amazon_testcases/TC_Amazon_validating_Login_005</testCaseId>
+      <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
+   </testCaseLink>
+</TestSuiteEntity>
