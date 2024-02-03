9e047f4f9305a394cba761de8930181e1e84e180
ennap <ennap@LAPTOP-LI9S114J>
2024-02-03 21:17:15 +0530

    Added a New Project1

---
+++ Test Suites/DDT_TestSuit/Testlistener_Testsuites/TC_Amazon_validating_ListOfCountries_footerlinks-003.ts
@@ -0,0 +1,21 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description></description>
+   <name>TC_Amazon_validating_ListOfCountries_footerlinks-003</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>3</numberOfRerun>
+   <pageLoadTimeout>30</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <rerunImmediately>true</rerunImmediately>
+   <testSuiteGuid>363de598-27d7-4ed2-a908-9a3c0b94b1cc</testSuiteGuid>
+   <testCaseLink>
+      <guid>f0012cc7-8ea6-48cc-8d73-1522ccf46fb1</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Amazon_testcases/TC_Amazon_validating_ListOfCountries_footerlinks-003</testCaseId>
+      <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
+   </testCaseLink>
+</TestSuiteEntity>
